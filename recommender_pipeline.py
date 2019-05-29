c69a1070a972ed56e01d3c23e6264938712cf277
Venkateswaran Shekar <vshekar@vshekar-19.local>
2019-05-29 11:55:53 -0400

    Added grid_params

--- recommender_pipeline.py
+++ recommender_pipeline.py
@@ -1,4 +1,5 @@
 from itertools import product
+import numpy as np
 
 grid_params = {
     'reaction_temperature': [100.1, 120.2, 43.3],
@@ -14,6 +15,10 @@ grid_params = {
 
 grid_params_descs = list(grid_params.keys())
 grid_params_desc_vals = list(grid_params.values())
-grid_params_desc_combos = product(*grid_params_desc_vals)
+#grid_params_desc_combos = product(*grid_params_desc_vals)
 
-print(list(grid_params_desc_combos))
+#print(len([combo for combo in grid_params_desc_combos]))
+
+grid_numpy_vals = [np.array(val) for val in grid_params_desc_vals]
+result = np.meshgrid(*grid_numpy_vals, sparse=False, indexing='ij')
+print(result[0])
